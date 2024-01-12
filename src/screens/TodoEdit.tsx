bfbd5f43179eca0a9899afb7822c7ad4ec386834
Arijan Jašarević <arijan.jasarevic@superius.hr>
2024-01-12 12:49:29 +0100

    Removed unused import

--- src/screens/TodoEdit.tsx
+++ src/screens/TodoEdit.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { ITodo } from "../types/todo";
-import { renderToReadableStream } from "react-dom/server";
 
 type TodoEditParams = {
   todo_id: string;
